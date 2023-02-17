7eb153e56bb4099674247a3535eada8274621b53
Tonny Githinji Macharia <githinit52@gmail.com>
2023-02-17 07:16:28 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /*
- * main - Prints the size of various types based on
- * the computer it is compiled and run on ..
+ * main - Entry point
+ * Description: "Prints the size of various types on the computer it is compiled and run on." 
  * Return: Always 0.
  */
 
