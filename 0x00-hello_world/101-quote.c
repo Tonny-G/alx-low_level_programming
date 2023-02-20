cbb14c3d25fe1c882e7b0635b0b6cefd3d154b58
Tonny Githinji Macharia <githinit52@gmail.com>
2023-02-20 12:57:52 -0800

    last

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - prints "and tha piece of art is useful" -Dora korpar 2015-10-19",
+ * followed by a new line
+ * Return: Always 1.
+ */
+
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora korpor 2015-10-19\n",59);
+	return (1);
+}
