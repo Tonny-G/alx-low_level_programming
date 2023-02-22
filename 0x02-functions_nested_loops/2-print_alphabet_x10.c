2eff825103e21d770a3c95b63be089b3a37d9920
Tonny Githinji Macharia <githinit52@gmail.com>
2023-02-22 00:24:18 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the aplhabet, in lowercase
+ * followed by new line
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i=0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+	i++;
+	}
+}
+
